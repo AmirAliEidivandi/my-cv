d4050167906f2e95165ccb4f3058f7e12e145b9d
AmirAli6578 <amirah648887@gmail.com>
2021-12-08 17:05:13 +0330

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector("#menu-btn");
+let header = document.querySelector(".header");
+let themeToggler = document.querySelector("#theme-toggler");
+
+// toggle menu
+menu.addEventListener("click", () => {
+    menu.classList.toggle("fa-times");
+    header.classList.toggle("active");
+});
+
+// theme toggler
+themeToggler.addEventListener("click", () => {
+    themeToggler.classList.toggle("fa-sun");
+    if (themeToggler.classList.contains("fa-sun")) {
+        document.body.classList.add("active");
+    } else {
+        document.body.classList.remove("active");
+    }
+});
